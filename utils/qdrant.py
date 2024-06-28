59463d8275848ca9800281979caf80019caf4006
user8171 <arsf32303@gmail.com>
2024-06-28 11:00:49 +0800

    1.8.2 不再使用docker 管理qdrant container...

--- utils/qdrant.py
+++ utils/qdrant.py
@@ -1,7 +1,9 @@
 import os
-import docker
+import sys
+import requests
 import gradio as gr
 import pandas as pd
+from sympy import EX
 from utils.logging_colors import logger
 from qdrant_client import QdrantClient, models
 
@@ -14,32 +16,16 @@ embedding_model_list = ['BAAI/bge-small-zh-v1.5', 'intfloat/multilingual-e5-larg
 
 class Qdrant:
     def qdrant_start_db() -> None :
+        # check qdrant connection
         try:
-            client = docker.DockerClient(base_url=os.getenv("DOCKER_SOCKET_URL", f"unix:///home/{os.getlogin()}/.docker/desktop/docker.sock"))
+            requests.get(os.getenv("QDRANT_URL", "http://192.168.1.72:6333"),
+                    timeout=(10, None)).status_code != 200
+        except requests.exceptions.ConnectionError:
+            logger.error("Qdrant connect refuse.")
+            sys.exit(1)
         except Exception as e:
-            logger.error("Failed to start Qdrant container, Error: " + str(e))
-            gr.Error("無法啟動Qdrant容器.")
-            return False
-        
-        if client.containers.list(all=True, filters={"name": ["qdrant"]}) == []:
-            logger.info("container not exist, creating container...")
-            try:
-                client.containers.run(
-                    "qdrant/qdrant:latest",
-                    name="qdrant",
-                    ports={"6333/tcp": 6333, "6334/tcp": 6334},
-                    detach=True,
-                    volumes={f"{os.getcwd()}/qdrant_data": {"bind": "/qdrant/storage", "mode": "rw"}}
-                )
-            except Exception as e:
-                logger.error("Failed to start Qdrant container, Error: " + str(e))
-                gr.Error("無法啟動Qdrant容器.")
-                return False
-            
-        else:
-            logger.info("found exist qdrant container, starting container...")
-            client.containers.get("qdrant").start()
-            # time.sleep(5)
+            logger.error(f"Qdrant connection failed. Reason: {e}")
+            sys.exit(1)
         
         collection_list = []
         collections = qdrant_client.get_collections()
